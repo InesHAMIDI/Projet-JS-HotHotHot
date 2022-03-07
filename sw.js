e93e20ca6d7296ff673e676624fc049e4571ca8d
Florian70 <florian.grosjean@etu.univ-amu.fr>
2022-03-07 08:11:37 +0000

    c'est mon anniv

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 self.addEventListener('install', (e) => {
     e.waitUntil(
       caches.open('HotHotHot').then((cache) => cache.addAll([
-        'index.html',
-        'app.js',
-        'images/duck.jpg',
+        '/Projet-JS-HotHotHot/index.html',
+        '/Projet-JS-HotHotHot/app.js',
+        '/Projet-JS-HotHotHot/images/duck.jpg',
       ])),
     );
   });
