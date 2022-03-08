3242fd3be4d584e56b223bc8c324ab0b045d2c51
Yvan Haspekian <72599017+Yvan-Haspekian@users.noreply.github.com>
2022-03-08 17:43:48 +0100

    Ça avance bien
    Calcul de valeur minimum et maximum du capteur
    Automatisation de la lecture des capteurs et de l'actualisation de la page
    Ajout d'une simulation de capteurs
    Mise en fonctionnement des deux méthodes de récupération de données (wss/fetch et simulation) : en cours

--- scripts/Sensor.js
+++ scripts/Sensor.js
@@ -6,6 +6,24 @@ class Sensor
 		this.type = type;
 		this.location = location;
 		this.data = new Array();
+
+		let sensors = document.getElementById("sensors");
+
+        this.htmlDiv       = document.createElement("div");
+        this.htmlP         = document.createElement("p");
+        this.htmlUl        = document.createElement("ul");
+        this.htmlType      = document.createElement("li");
+        this.htmlLocation  = document.createElement("li");
+        this.htmlTimestamp = document.createElement("li");
+        this.htmlValue     = document.createElement("li");
+
+        sensors.appendChild(this.htmlDiv);
+        this.htmlDiv.appendChild(this.htmlP);
+        this.htmlDiv.appendChild(this.htmlUl);
+        this.htmlUl.appendChild(this.htmlType); 
+        this.htmlUl.appendChild(this.htmlLocation);
+        this.htmlUl.appendChild(this.htmlTimestamp);
+        this.htmlUl.appendChild(this.htmlValue);
 	}
 
 	getData()
@@ -33,7 +51,7 @@ class Sensor
 		return todayData;
 	}
 
-	getMinValue(today)
+	getTodayMinValue()
 	{
 		let todayData = this.getTodayData();
 		let todayValues = new Array();
@@ -41,18 +59,46 @@ class Sensor
 		for(let i = 0; i < todayData.length; i++)
 			todayValues.push(todayData[i][1]);
 
-		let minValueIndex = todayValues.indexOf(Math.min.apply(null, todayValues));
+		let minValue = Math.min.apply(null, todayValues);
+		let minValueIndex = -1;
 
-		return todayData[minValueIndex];
+		for(let i = 0; i < todayValues.length; i++)
+		{
+			if(todayValues[i] == minValue)
+			{
+				minValueIndex = i;
+				break;
+			}
+		}
+
+		return todayValues[minValueIndex];
 	}
-	getMaxValue(today)
+	getTodayMaxValue()
 	{
-		return Math.max(this.data[1]);
+		let todayData = this.getTodayData();
+		let todayValues = new Array();
+
+		for(let i = 0; i < todayData.length; i++)
+			todayValues.push(todayData[i][1]);
+
+		let maxValue = Math.max.apply(null, todayValues);
+		let maxValueIndex = -1;
+
+		for(let i = 0; i < todayValues.length; i++)
+		{
+			if(todayValues[i] == maxValue)
+			{
+				maxValueIndex = i;
+				break;
+			}
+		}
+
+		return todayValues[maxValueIndex];
 	}
 
 	display()
 	{
-		let sensors = document.getElementById("sensors");
+		/*let sensors = document.getElementById("sensors");
 
         let div          = document.createElement("div");
         let p            = document.createElement("p");
@@ -78,6 +124,16 @@ class Sensor
         {
         	timestamp.innerHTML = new Date(this.data[this.data.length-1][0]).toDateString();
         	value.innerHTML     = this.data[this.data.length-1][1];
+        }*/
+
+        this.htmlP.innerHTML        = this.name;
+        this.htmlType.innerHTML      = this.type;
+        this.htmlLocation.innerHTML = this.location;
+
+        if(this.data[this.data.length-1] !== undefined)
+        {
+        	this.htmlTimestamp.innerHTML = new Date(this.data[this.data.length-1][0]).toDateString();
+        	this.htmlValue.innerHTML     = this.data[this.data.length-1][1];
         }
 	}
 };
