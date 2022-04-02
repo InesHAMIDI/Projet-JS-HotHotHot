01201184efdd2768b03934cdd2c0e210e360a9b0
InesHAMIDI <ines.hamidi@etu.univ-amu.fr>
2022-04-02 14:10:27 +0200

    un peu modif la partie SW registration du app.js mais trql

--- app.js
+++ app.js
@@ -1,35 +1,45 @@
-if ('serviceWorker' in navigator) {
+/*if ('serviceWorker' in navigator) {
     // Register a service worker hosted at the root of the
     // site using the default scope.
     navigator.serviceWorker.register('sw.js').then(
         (registration) => {
             console.log('Service worker registration succeeded:', registration)
         },
-        /*catch*/ (error) => {
-            console.log('Service worker registration failed:', error)
-        }
-    )
+       (error) => {
+    console.log('Service worker registration failed:', error)
+}
+)
 } 
 
 else {
     console.log('Service workers are not supported.')
-}
+}*/
 
+//pour les notifs
 function  main(){
     const permission = document.getElementById('push-permission')
     if(!permission || 
         !('Notification' in window) ||
-        !('ServiceWorker' in navigator)){
+        !('ServiceWorker' in navigator) ||
+        Notification.permission !== 'default'){
         return;
     }
     const button = document.createElement('button')
     button.innerText = 'Recevoir les notificatins'
     permission.appendChild(button)
-    button.addEventListener('click')
+    button.addEventListener('click', askPermission)
 }
 
-function askPermission(){
-    const permission =  Notification.requestPermission()
+async function askPermission(){
+    const permission = await Notification.requestPermission()
+    if(permission === 'granted'){
+        registerServiceWorker()
+    }
+}
+async function registerServiceWorker(){
+    const registration = await navigator.serviceWorker.register('sw.js')
+    const subscription = await registration.pushManager.getSubscription();
+    console.log(subscription);
 }
 
 main()
